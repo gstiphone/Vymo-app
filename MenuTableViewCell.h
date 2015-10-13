8eee67dc3413b54eede2b65d1808b90414c9fca0
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-10-13 12:52:56 +0530

    Create MenuTableViewCell.h

---
+++ MenuTableViewCell.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *lblMenu;
+@property (weak, nonatomic) IBOutlet UILabel *lblUserName;
+@property (weak, nonatomic) IBOutlet UILabel *lblLead;
+@property (weak, nonatomic) IBOutlet UILabel *lblCompanyName;
+@property (weak, nonatomic) IBOutlet UILabel *lblCompanyPerson;
+
+@end
