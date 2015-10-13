bccc488ce0ec4d0ccee962a6c1e7c11beb2f8877
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-10-13 12:48:00 +0530

    Update CalenderVW.h

--- CalenderVW.h
+++ CalenderVW.h
@@ -1,5 +1,17 @@
-#import <UIKit/UIKit.h>
-
-@interface CalenderVW : UIViewController
+@interface CalendarViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+{
+    NSMutableData *lookServerResponseData;
+    NSArray *arrData;
+    IBOutlet UITableView *tbl;
+    UISwipeGestureRecognizer *swipeLeft;
+    UISwipeGestureRecognizer *swipeRight;
+    NSMutableArray *arrMenu;
+    IBOutlet UITableView *tblMenu;
+    BOOL isSwipe;
+    NSDictionary* responseDict;
+}
+@property (weak, nonatomic) IBOutlet UIView *firstView;
+@property (weak, nonatomic) IBOutlet UIView *secondView;
+- (IBAction)btn_menue:(id)sender;
 
 @end
