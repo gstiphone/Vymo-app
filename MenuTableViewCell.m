7bfd2d7afb66ca6e5ff43858ccd49e7c42b0f69a
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-10-13 12:53:50 +0530

    Create MenuTableViewCell.m

---
+++ MenuTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MenuTableViewCell.m
+//  Vymo_test
+//
+//  Created by Administrator on 26/09/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import "MenuTableViewCell.h"
+
+@implementation MenuTableViewCell
+@synthesize lblMenu;
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
