998f59184e933d486f6ad4bbdda9ac90f800b774
iosDevelopmentTeam <sabyasachi@gstcloud.com>
2015-10-26 14:35:50 +0530

    Update and rename SettingsViewController.h to UpdateHistory.h

---
+++ UpdateHistory.h
@@ -0,0 +1,20 @@
+//
+//  UpdateHistory.h
+//  Vymo_test
+//
+//  Created by Administrator on 12/10/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpdateHistory : UIViewController
+{
+    NSDictionary * myDictionary;
+}
+
+- (IBAction)btnBack:(id)sender;
+
+
+@end
+
